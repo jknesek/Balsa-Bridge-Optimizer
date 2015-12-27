ffa9fd7a13ded86c3b37064010634ccc8dd2e08c
Jack Knesek <jack.knesek@gmail.com>
2015-12-27 13:56:16 -0600

    Points Changed

--- tests/five_point_arch.py
+++ tests/five_point_arch.py
@@ -160,28 +160,28 @@ def get_truss_load_and_efficiency(the_truss, logger):
 
 def get_tuples():
     print "Getting All Tuples..."
-    x1 = numpy.arange (3.5, 5.5, 0.5)
-    y1 = numpy.arange (7.5, 10.0, 0.5)
+    x1 = numpy.arange (3.0, 6.0, 0.5)
+    y1 = numpy.arange (7.0, 10.5, 0.5)
     p1 = itertools.product (x1,y1)
     l1 = list(p1)
 
-    x2 = numpy.arange (8.0, 11.0, 0.5)
-    y2 = numpy.arange (13.5, 15.5, 0.5)
+    x2 = numpy.arange (5.5, 11.5, 0.5)
+    y2 = numpy.arange (14.0, 17.0, 0.5)
     p2 = itertools.product (x2,y2)
     l2 = list(p2)
 
-    x3 = numpy.arange (15.5, 18.5, 0.5)
-    y3 = numpy.arange (17.5, 19.5, 0.5)
+    x3 = numpy.arange (15.0, 19.0, 0.5)
+    y3 = numpy.arange (17.0, 20.0, 0.5)
     p3 = itertools.product (x3,y3)
     l3 = list(p3)
 
-    x4 = numpy.arange (26.0, 27.5, 0.5)
-    y4 = numpy.arange (15.5, 18.5, 0.5)
+    x4 = numpy.arange (23.5, 28.0, 0.5)
+    y4 = numpy.arange (15.0, 19.0, 0.5)
     p4 = itertools.product (x4,y4)
     l4 = list(p4)
 
-    x5 = numpy.arange (29.5, 31.5, 0.5)
-    y5 = numpy.arange (11.5, 13.5, 0.5)
+    x5 = numpy.arange (29.0, 32.0, 0.5)
+    y5 = numpy.arange (11.0, 14.0, 0.5)
     p5 = itertools.product (x5,y5)
     l5 = list(p5)
 
